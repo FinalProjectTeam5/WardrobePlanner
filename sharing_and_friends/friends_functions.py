f7b454be5174bb1d2bbe6b36b599e643fa05d2dd
allwrightgemma <gemma.allwright@gmail.com>
2023-05-18 12:06:17 +0100

    updated functions

--- sharing_and_friends/friends_functions.py
+++ sharing_and_friends/friends_functions.py
@@ -3,36 +3,58 @@ add friend - check for logged in user. check database for friend user_id
 """
 
 
-def add_friend(friend_username):
-    accept_decline = input("Do you want to accept friend request from {}? Y or N ".format(friend_username))
-    if accept_decline == "Y":
-        add_to_friend_list(friend_id)
+def add_friend(username):
+    friend_username = input("What is the username of the friend you would like to add?")
+    friend_id = DBSearch.get_friend_user_id(friend_username)
+    user_id = DBSearch.get_user_id(username)
+    DBSearch.add_to_friend_list(user_id, friend_id)
+    print("{} has been added to your friend list.".format(friend_username))
+
+
+def delete_friend(username):
+    friend_username = input("What is the username of the friend you would like to delete?")
+    friend_id = DBSearch.get_friend_user_id(friend_username)
+    DBSearch.delete_from_friend_list(friend_id)
+    print("{} has been deleted from your friend list.".format(friend_username))
+
+
+def get_friend_user_id(friend_id):
+    try:
+        db_connection = connect_to_db()
+        cursor = db_connection.cursor()
+        query = "SELECT user_ID from users WHERE username = {}".format(friend_id)
+        cursor.execute(query)
+    except Exception:
+        raise NoConnection
     else:
-        return "{} has not been added to your friend list".format(friend_username)
-
-
-# def get_user_id():
-#     try:
-#         db_connection = connect_to_db()
-#         cursor = db_connection.cursor()
-#         query = "SELECT user_ID from users"
-#         cursor.execute(query)
-#     except Exception:
-#         raise NoConnection
-#     else:
-#         result = cursor.fetchall()
-#         user_id = result
-#         return user_id
-#     finally:
-#         if db_connection:
-#             db_connection.close()
+        result = cursor.fetchall()
+        user_id = result
+        return user_id
+    finally:
+        if db_connection:
+            db_connection.close()
 
 
 def add_to_friend_list(user_id, friend_id):
     try:
         db_connection = connect_to_db()
         cursor = db_connection.cursor()
-        query = "INSERT INTO friends (user_id, friend_id) VALUES ({}, {})".format(user_ID, friend_ID)
+        query = "INSERT INTO friends (userid, friendid) VALUES ({}, {})".format(user_id, friend_id)
+        cursor.execute(query)
+    except Exception:
+        raise NoConnection
+    else:
+        db_connection.commit()
+    finally:
+        if db_connection:
+            db_connection.close()
+
+
+def delete_from_friend_list(friend_id):
+    try:
+        db_connection = connect_to_db()
+        cursor = db_connection.cursor()
+        query = "DELETE FROM friends AS f WHERE f.friendid = {})".format(friend_id)
         cursor.execute(query)
     except Exception:
         raise NoConnection
@@ -47,7 +69,10 @@ def add_to_friend_list(user_id, friend_id):
 Check status, if available, change to unavailable. If not available return cannot be borrowed.
 """
 
-clothing_id = 4
+def get_item_availability():
+    item_description = input("What is the item you would like to borrow?")
+    item_id = DBSearch.get_item_id(item_description)
+    DBSearch.check_availability(item_id)
 
 
 def check_availability(item_id):
@@ -64,7 +89,7 @@ def check_availability(item_id):
         if result == "available":
             db_connection = connect_to_db()
             cursor = db_connection.cursor()
-            query = "UPDATE availability_status SET availability_status.item_status = 'unavailable' WHERE availability_status.item_id = {}".format(item_id)
+            query = "UPDATE availability_status AS a SET a.item_status = 'taken' WHERE a.item_id = {}".format(item_id)
             cursor.execute(query)
             return "This item is ready to be shared with you"
         else:
