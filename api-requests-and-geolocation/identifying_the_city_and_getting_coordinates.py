a6d0ba9f593d37826d1645b31efbfa6ceb0b638c
kin-b <109428751+kin-b@users.noreply.github.com>
2023-05-20 12:27:04 +0200

    Update identifying_the_city_and_getting_coordinates.py

--- api-requests-and-geolocation/identifying_the_city_and_getting_coordinates.py
+++ api-requests-and-geolocation/identifying_the_city_and_getting_coordinates.py
@@ -1,45 +1,37 @@
-import itertools
-from pprint import pprint
-from geopy.geocoders import Nominatim
+from functions import check_city, city_list_for_interface, cleaning_up_results, hometown_input_check
+
+hometown = None
+
+user_typed_hometown = input("If you want to use the weather tags in your wardrobe searches please provide your hometown.\n"
+                    "  - type the the town name in your native tongue or in a Latin transcription\n"
+                    "  - if the api didn't find your hometown on first attempt, try the name of the nearest bigger town instead\n"
+                    "\n"
+                    "Your hometown: ")
 
 
-#looks up the provided city, returns a geopy location object of matches or a None object
-def check_city(city):
-    geolocator = Nominatim(user_agent="MyApp")
-    location = geolocator.geocode(city, exactly_one=False)
-    return location
+user_hometown_choice = hometown_input_check(user_typed_hometown)
 
 
-def cleaning_up_results(raw_list):
-    new_list = [list(item) for item in raw_list]  # converts the geopy object into a list
-    lat_long_list = [item[1] for item in new_list]  # creates a list of latitude, longitude tuples
-    new_list = [item[0] for item in new_list]  # removes the latitude, longitude tuples from the previous list
-    duplicate_finder = []
-    compressor = [False for item in range(len(new_list))]  # creates a starter second list for the compress method
-    # turns the location information into a list
-    for item in new_list:
-        item = item.split(", ")
-        if len(item) > 1:
-            duplicate_finder.append(item[1])
-    # prepares the proper 2nd list for the compress method
-    for item in duplicate_finder:
-        index = duplicate_finder.index(item)
-        compressor[index] = True
-    city_list = list(itertools.compress(new_list, compressor))
-    print(city_list)
-    lat_long_list = list(itertools.compress(lat_long_list, compressor))
-    # combines back the city name and the coordinates but only for unique cities
-    unique_cities_with_coordinates = list(zip(city_list, lat_long_list))
-    return unique_cities_with_coordinates
+if user_hometown_choice:
+    possible_cities = check_city(user_typed_hometown)
+    list_prep = cleaning_up_results(possible_cities)
+    city_list = city_list_for_interface(list_prep)
+    print("Choose your hometown from the list:")
+    for city in city_list:
+        print(city)
+else:
+    hometown = None
 
+while True:
+    hometown = input("\n_")
+    if hometown.isdigit() and hometown in range(1, len(city_list)):
+        break
+    else:
+        print("Please choose an option from the list")
 
-def city_list_for_interface():
-    pass
+if hometown == len(city_list):
+    hometown = None
 
-
-# hometown = input("What is your home town, or the closest town to where you live? ")
-# print(type(check_city(hometown)))
-# print(check_city(hometown))
 
 
 # raw_results = check_city("Warsaw")
