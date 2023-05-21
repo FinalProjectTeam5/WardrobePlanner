756f7b12374e28ba8d5f0c14f7e384e7a868a322
Katarzyna Wojtaszczyk <sdkasik152@gmail.com>
2023-05-21 19:25:23 +0200

    Added a test file and directory

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,18 +1,2 @@
 import unittest
 import login
-from db_utils import DBSearch
-
-
-def get_new_username():
-    username = input("Your username: ")
-    if len(username) < 2 or len(username) > 20:
-        print("Sorry, your username should be between 2 and 20 characters long.\n Try again.")
-        return get_new_username()
-    elif DBSearch.username_check(username):
-        print("You already have an account here. Login here: ")
-        return login()
-    else:
-        return username
-
-
-print(get_new_username())
