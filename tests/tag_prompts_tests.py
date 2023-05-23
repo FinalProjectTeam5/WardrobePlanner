8e6adb4523aa2a4fd55dff4736e979ffb6b8ef2f
magdazembik <30858095+magdazembik@users.noreply.github.com>
2023-05-23 23:04:58 +0200

    Update tag_prompts_tests.py
    addedd whether_weather test

--- tests/tag_prompts_tests.py
+++ tests/tag_prompts_tests.py
@@ -3,7 +3,7 @@ from unittest import TestCase, mock
 from WardrobePlanner.functions.tag_prompts import users_choices, whether_weather
 
 
-class Test_TagPrompts(TestCase):
+class Test_WhetherWeather(TestCase):
     def test_whether_weather_valid_input_y(self):
         with mock.patch('builtins.input', return_value='y'):
             self.assertTrue(whether_weather())
@@ -13,9 +13,10 @@ class Test_TagPrompts(TestCase):
             self.assertFalse(whether_weather())
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_whether_weather_invalid_input(self):
+        with mock.patch('builtins.input', side_effect=['a', '123', 'Y', 'N']):
+            with self.assertRaises(ValueError):
+                whether_weather()
 
 
 if __name__ == '__main__':
