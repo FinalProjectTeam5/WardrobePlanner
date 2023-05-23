4df0fd88942ec437cb582bac365eaf3358229a0e
magdazembik <30858095+magdazembik@users.noreply.github.com>
2023-05-23 19:10:54 +0200

    Update tag_prompts_tests.py

--- tests/tag_prompts_tests.py
+++ tests/tag_prompts_tests.py
@@ -1,1 +1,23 @@
+from unittest import TestCase, mock
+from WardrobePlanner.functions.tag_prompts import users_choices, whether_weather
 
+class Test_TagPrompts(TestCase):
+    def test_whether_weather_valid_input_y(self):
+        with mock.patch('builtins.input', return_value='y'):
+            self.assertTrue(whether_weather())
+
+    def test_whether_weather_valid_input_n(self):
+        with mock.patch('builtins.input', return_value='n'):
+            self.assertFalse(whether_weather())
+
+
+            
+            
+            
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
