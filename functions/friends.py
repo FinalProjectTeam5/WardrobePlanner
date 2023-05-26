27f9fa9af4daa8cac559c18ba67f40b5fe918e0f
allwrightgemma <gemma.allwright@gmail.com>
2023-05-26 15:31:31 +0100

    added try blocks to friends

--- functions/friends.py
+++ functions/friends.py
@@ -2,17 +2,18 @@ from WardrobePlanner.classes.db_utils import DBSearch
 
 
 def add_friend(user_id):
-    friend_username = input("What is the username of the friend you would like to add? \n")
-    friend_id = DBSearch.get_friend_user_id(friend_username)[0][0]
-    friends_list = DBSearch.get_friends_list(user_id)
-    for i in range(len(friends_list)):
-        if friend_id in friends_list[i]:
-            return "You are already friends with {}.".format(friend_username)
-    if friend_id == "NULL":
-        return "Sorry, that user doesn't exist"
-    else:
-        DBSearch.add_to_friend_list(user_id, friend_id)
-        return "{} has been added to your friend list.".format(friend_username)
+    try:
+        friend_username = input("What is the username of the friend you would like to add? \n")
+        friend_id = DBSearch.get_friend_user_id(friend_username)[0][0]
+        friends_list = DBSearch.get_friends_list(user_id)
+        for i in range(len(friends_list)):
+            if friend_id in friends_list[i]:
+                return "You are already friends with {}.".format(friend_username)
+        else:
+            DBSearch.add_to_friend_list(user_id, friend_id)
+            return "{} has been added to your friend list.".format(friend_username)
+    except IndexError:
+        print("Sorry, that isn't a valid user. Please try again.")
 
 
 def show_friends_list(user_id):
@@ -21,9 +22,12 @@ def show_friends_list(user_id):
 
 
 def delete_friend(user_id):
-    print(show_friends_list(user_id))
-    friend_username = input("What is the username of the friend you would like to delete? \n")
-    friend_id = DBSearch.get_friend_user_id(friend_username)[0][0]
-    DBSearch.delete_from_friend_list(friend_id, user_id)
-    return "{} has been deleted from your friend list.".format(friend_username)
+    try:
+        print(show_friends_list(user_id))
+        friend_username = input("What is the username of the friend you would like to delete? \n")
+        friend_id = DBSearch.get_friend_user_id(friend_username)[0][0]
+        DBSearch.delete_from_friend_list(friend_id, user_id)
+        return "{} has been deleted from your friend list.".format(friend_username)
+    except IndexError:
+        print("Sorry, that isn't a valid user. Please try again.")
 
