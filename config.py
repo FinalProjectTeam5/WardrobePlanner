9056a3ca0987b49081b464f3674c797269ad59b9
kin-b <109428751+kin-b@users.noreply.github.com>
2023-05-28 22:59:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# for now your personal host, root and password from your machine, the ones you used during class in foundation unless you've changed it since then
-
 HOST = "localhost"
 USER = "root"
 PASSWORD = "password"
